267b7c5cf2f7917ba144cf5098e7ea11a01f622e
Nathan Henniges <demonwolf@demonwolfdev.com>
2018-10-23 16:38:09 -0500

    Added more.

---
+++ pages/settings.php
@@ -0,0 +1,38 @@
+<div id="wrap">
+    <h1><?php _e('Livestream Notice Settings', 'livestreamNotice'); ?></h1>
+
+    <form method="post" action="options.php">
+
+        <?php settings_fields($this->pluginPrefix . '-settings-group'); ?>
+        <?php do_settings_sections($this->pluginPrefix . '-settings-group'); ?>
+
+        <table class="form-table">
+            <tbody>
+            <tr valign="top">
+                <th scope="row"><label
+                        for="<?php echo $this->pluginPrefix; ?>-channelname"><?php _e('Twitch Channel', 'livestreamNotice'); ?></label></th>
+                <td><input name="<?php echo $this->pluginPrefix; ?>-channelname" type="text" id="<?php echo $this->pluginPrefix; ?>-channelname" value="<?php echo get_option($this->pluginPrefix .  '-channelname',$this->channelname) ?>" class="regular-text">
+
+                    <p class="description"><?php _e('Your Twitch account username you want to show notices for on your website.', 'livestreamNotice'); ?></p>
+                </td>
+            </tr>
+
+            <tr valign="top">
+                <th scope="row"><label
+                        for="<?php echo $this->pluginPrefix; ?>-twitchclientid"><?php _e('Twitch Client ID', 'livestreamNotice'); ?></label></th>
+                <td><input name="<?php echo $this->pluginPrefix; ?>-twitchclientid" type="text" id="<?php echo $this->pluginPrefix; ?>-twitchclientid" value="<?php echo get_option($this->pluginPrefix .  '-twitchclientid',$this->twitchclientid) ?>" class="regular-text">
+                <h4>How to get your twitch client ID</h4>
+                <div>1.Go to the <a href="https://dev.twitch.tv/" target="_blank">Twitch Dev site</a> and log in to your account.</div>
+                <div>2.Under My Applications, choose Register an App.</div>
+                <div>3. On the Dashboard, under Developer Applications, choose Register Your Application.</div>
+                <div>4. On the Register Your Application page, complete the form and choose Register.</div>
+                <div>5.Note the generated client ID that you will use to paste into the <strong>Twitch Client ID field</strong></div>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+
+        <?php submit_button(); ?>
+
+    </form>
+</div>
---
+++ readme.md
@@ -0,0 +1,69 @@
+=== Livestream Notice ===
+
+Tags: twitch,notify, notices,livestream
+
+Requires at least: 4.6
+
+Tested up to: 4.9.8
+
+Stable tag: 4.9.8
+
+Requires PHP: 5.2.4
+
+License: GPLv2 or later
+
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+
+Displays a notify type message of twitch channel name when they are live. Great if you're a streamer with a WordPress Blog and or if you're a programmer who wants to share you're live.
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+e.g.
+
+1. Upload the plugin files to the `/wp-content/plugins/` directory, or install the plugin through the WordPress plugins screen directly.
+
+2. Activate the plugin through the 'Plugins' screen in WordPress
+
+3. Use the Settings-&gt;Livestream Notice screen to configure the plugin
+
+4.Go to the https://dev.twitch.tv/ and log in to your account.
+
+5.Under My Applications, choose Register an App.&lt;
+
+6. On the Dashboard, under Developer Applications, choose Register Your Application.
+
+7. On the Register Your Application page, complete the form and choose Register.
+
+8.Note the generated client ID that you will use to paste into the Twitch Client ID field
+
+== Screenshots ==
+
+1. Basic Settings Panel
+
+2. Example of the notice
+
+== Changelog ==
+
+= 1.1.3 =
+
+* Found a few more grammar issues which was in the description.
+
+= 1.1.2 =
+
+* Someone pointed out a grammar errors.
+
+= 1.1.1 =
+
+* Missed typed the author URL.
+
+= 1.1 =
+
+* Fixed a few typos and added more infomation
+
+= 1.0 =
+
+* Initial Release
